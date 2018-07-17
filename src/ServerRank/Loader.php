03cdf2e8ed04421fc5fdeda636b194e1be6830a3
KimSangYun <32999975+UnixTeamAdmin@users.noreply.github.com>
2018-07-17 21:48:23 +0900

    All Fix

--- src/ServerRank/Loader.php
+++ src/ServerRank/Loader.php
@@ -16,13 +16,13 @@ use ServerRank\Task\{
 class Loader extends PluginBase{
 
 	/** @var string */
-	private $prefix = '§d<§f시스템§d>§f ';
+	public $prefix = '§d<§f시스템§d>§f ';
 
 	/** @var Loader */
 	private static $instance = null;
 
 	/** @var array */
-	private $servers;
+	private $serverinfo;
 
 	/** @var int */
 	private $requestCount = 0;
@@ -32,6 +32,9 @@ class Loader extends PluginBase{
 
 	/** @var int */
 	private $repeatingInterval = 6000; // 5 minutes
+
+	/** @var bool */
+	public $Compulsion = false;
 
 	/**
 	* @return Loader
@@ -57,19 +60,30 @@ class Loader extends PluginBase{
 	}
 
 	public function getServerRankTaskCompulsionRegister(array $servers){
-		$serverss = [];
-		$serverss['address'] = [];
-		$serverss['address']['host'] = Utils::getIp();
-		$serverss['address']['port'] = $this->getServer()->getPort();
-		$serverss['numplayers'] = count($this->getServer()->getOnlinePlayers());
-		array_push($servers, $serverss);
+		$register = [];
+		$register['address'] = [];
+		$register['address']['host'] = Utils::getIp();
+		$register['address']['port'] = $this->getServer()->getPort();
+		$register['numplayers'] = count($this->getServer()->getOnlinePlayers());
+		array_push($servers, $register);
 		usort($servers, function($a, $b){
-			return $a['numplayers'] > $b['numplayers'] ? 1 : -1;
+			return $a['numplayers'] < $b['numplayers'] ? 1 : -1;
 		});
-		$this->getServerRankTaskRegister($servers);
+		foreach($servers as $i => $server){
+			$rank = $i + 1;
+			$this->serverinfo[$server['address']['host'] . ':' . $server['address']['port']] = $rank;
+		}
+		foreach($servers as $server){
+			++$this->requestCount;
+			$this->getServer()->getAsyncPool()->submitTask(new GetInfoTask($server['address']['host'], $server['address']['port'], $servers));
+		}
 	}
 
 	public function getServerRankTaskRegister(array $servers): void {
+		foreach($servers as $i => $server){
+			$rank = $i + 1;
+			$this->serverinfo[$server['address']['host'] . ':' . $server['address']['port']] = $rank;
+		}
 		foreach($servers as $server){
 			++$this->requestCount;
 			$this->getServer()->getAsyncPool()->submitTask(new GetInfoTask($server['address']['host'], $server['address']['port'], $servers));
@@ -77,29 +91,22 @@ class Loader extends PluginBase{
 	}
 
 	public function getHostByNameCallback(string $hostname, string $ip, int $port, $servers): void {
-		foreach($servers as $i => $server){
-			$rank = $i + 1;
-			$this->servers[$ip . ':' . $port] = $rank;
-		}
 		if($ip === Utils::getIP() and $port === $this->getServer()->getPort()){
-			if(isset($this->servers[$ip . ':' . $port])){
-				$updatedOnce = $this->rank !== null;
+			if(isset($this->serverinfo[$ip . ':' . $port])){
 
-				$this->rank = $this->servers[$ip . ':' . $port];
+				$this->rank = $this->serverinfo[$ip . ':' . $port];
 
 				$this->getServer()->getLogger()->notice($this->prefix . '한국 전체 서버 중 우리 서버의 순위는 §7(MCBE RANK 기준) §d§l' . $this->rank . '§r위 입니다.');
 
-				if($updatedOnce){
-					foreach($this->getServer()->getOnlinePlayers() as $player){
-						$this->sendRankMessage($player);
-					}
+				foreach($this->getServer()->getOnlinePlayers() as $player){
+					$this->sendRankMessage($player);
 				}
 			}
-		}else if(--$this->requestCount === 0){
+		}else if(--$this->requestCount === 0 && !$this->Compulsion){
 			$this->getServer()->getLogger()->alert($this->prefix . 'https://mcberank.kro.kr 사이트에 서버가 등록되어 있지 않습니다.');
 			$this->getServer()->getLogger()->alert($this->prefix . '카카오톡 아이디 solo5star, 검색 후 연락 주세요.');
 			$this->getServer()->getLogger()->alert($this->prefix . '등록이 되있지 않더라도 서버 순위가 계산이 되어 출력이 됩니다.');
-			$this->getServerRankTaskCompulsionRegister($servers);
+			$this->Compulsion = true;
 		}
 	}
 	public function sendRankMessage(Player $player): void {
